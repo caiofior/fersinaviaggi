cbbedc8f2e60905661f86a62e6f164f8d58a5edd
Claudio Fior <caiofior@gmail.com>
2013-09-06 16:59:56 +0200

    Support of background programm call

--- cercavoli/default.aspx.vb
+++ cercavoli/default.aspx.vb
@@ -1,4 +1,16 @@
 ﻿Partial Class FlySearch
     Inherits System.Web.UI.Page
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        'Dim process As System.Diagnostics.Process = New System.Diagnostics.Process()
+        'process.StartInfo.FileName = Server.MapPath("..\Bin\FlyBackGroundSearch.exe")
+        'process.StartInfo.WorkingDirectory = Server.MapPath("..\Bin")
+        'process.StartInfo.RedirectStandardOutput = True
+        'process.StartInfo.RedirectStandardError = True
+        'process.StartInfo.CreateNoWindow = True
+        'process.StartInfo.UseShellExecute = False
+        'process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden
+        'process.Start()
+
+    End Sub
 End Class
