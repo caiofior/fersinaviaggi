b36790655191a172bda53b367c44ad4d4f325f8f
Claudio Fior <caiofior@gmail.com>
2013-09-09 14:54:17 +0200

    First suppoert update method

--- App_Code/lib/core/Entity.vb
+++ App_Code/lib/core/Entity.vb
@@ -1,6 +1,8 @@
 ﻿Imports Microsoft.VisualBasic
 Imports MySql.Data.MySqlClient
 Imports System.Collections.Specialized
+Imports System.Globalization
+Imports System.Threading
 
 Namespace Libray.Core
     Public MustInherit Class Entity
@@ -10,10 +12,15 @@ Namespace Libray.Core
         Protected _primaryKey As String
         Protected _fields() As String
         Sub New(ByVal connection As MySql.Data.MySqlClient.MySqlConnection)
+            Dim newCulture As CultureInfo = System.Threading.Thread.CurrentThread.CurrentCulture.Clone()
+            newCulture.DateTimeFormat.ShortDatePattern = "yyyy-M-dd"
+            newCulture.DateTimeFormat.ShortTimePattern = "hh:mm"
+            newCulture.DateTimeFormat.DateSeparator = "-"
+            Thread.CurrentThread.CurrentCulture = newCulture
             _connection = connection
         End Sub
         Public Sub setData(ByVal field As String, ByVal value As String)
-            _data.Add(field, value)
+            _data.Set(field, value)
         End Sub
         Public Function getData(ByVal field As String) As String
             Return _data.Get(field)
@@ -27,8 +34,9 @@ Namespace Libray.Core
                 setData(reader.GetName(columCount).ToString, reader.GetValue(columCount).ToString)
             Next
             reader.Close()
+
         End Sub
-        Public Sub insert()
+        Public Overridable Sub insert()
             getMetadata()
             Dim sql As String = "INSERT INTO `" + _tablename + "` SET "
             Dim entityValue As String
@@ -51,11 +59,11 @@ Namespace Libray.Core
             Dim command As New MySqlCommand(sql, _connection)
             If _connection.State = ConnectionState.Closed Then _connection.Open()
             Dim reader As MySqlDataReader = command.ExecuteReader(CommandBehavior.CloseConnection)
-            reader.Close()
             sql = "SELECT LAST_INSERT_ID() FROM `" + _tablename + "`"
             command = New MySqlCommand(sql, _connection)
             If _connection.State = ConnectionState.Closed Then _connection.Open()
             reader = command.ExecuteReader(CommandBehavior.CloseConnection)
+            reader.Close()
             reader.Read()
             For columCount As Integer = 0 To reader.FieldCount - 1
                 setData(_primaryKey, reader.GetValue(columCount).ToString)
@@ -63,6 +71,33 @@ Namespace Libray.Core
             reader.Close()
 
         End Sub
+        Public Overridable Sub update()
+            getMetadata()
+            Dim sql As String = "UPDATE `" + _tablename + "` SET "
+            Dim entityValue As String
+            Dim c As Integer = 0
+            For Each entityValue In _data.AllKeys
+                If (Array.Exists(Of String)(_fields, Function(i) i = entityValue)) And entityValue <> _primaryKey Then
+                    If c > 0 Then
+                        sql = sql + ","
+                    End If
+                    Dim value As String = _data(entityValue)
+                    If value = "NULL" Or value = "NOW()" Then
+                        sql = sql + " `" + entityValue + "` = " + Util.sqlSanitize(value)
+                    Else
+                        sql = sql + " `" + entityValue + "` = """ + Util.sqlSanitize(value) + """"
+                    End If
+                    c = c + 1
+
+                End If
+            Next
+            sql = sql + " WHERE `" + _primaryKey + "` = """ + Util.sqlSanitize(getData(_primaryKey)) + """"
+            Dim command As New MySqlCommand(sql, _connection)
+            If _connection.State = ConnectionState.Closed Then _connection.Open()
+            Dim reader As MySqlDataReader = command.ExecuteReader(CommandBehavior.CloseConnection)
+            reader.Close()
+        End Sub
+
         Private Sub getMetadata()
             If _fields Is Nothing Then
                 Dim command As New MySqlCommand("SELECT * FROM " + _tablename + " LIMIT 0", _connection)
--- App_Code/lib/fly/Search.vb
+++ App_Code/lib/fly/Search.vb
@@ -9,13 +9,19 @@ Namespace Libray.Fly
             MyBase.New(connection)
             _tablename = "fly_search"
         End Sub
+        Public Overrides Sub update()
+            If (getData("arrival_datetime") = "") Then
+                setData("arrival_datetime", "NULL")
+            End If
+            MyBase.update()
+        End Sub
 
         Sub loadFromId(ByVal id As Integer)
             Dim sql As String = "SELECT * FROM " + _tablename + " WHERE id = """ + Util.sqlSanitize(id) + """"
-            Try
-                loadFromSql(sql)
-            Catch ex As Exception
-            End Try
+            'Try
+            loadFromSql(sql)
+            'Catch ex As Exception
+            'End Try
 
         End Sub
 
